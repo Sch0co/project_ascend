63c2887162f26dc888aa39cb5325aba71783e430
Sch0co <tnqlsgyqls98@naver.com>
2021-10-25 23:03:48 +0900

    main modify

--- src/view/Main/index.js
+++ src/view/Main/index.js
@@ -1,34 +1,66 @@
 import React from "react";
 import "./index.css";
-
+import { Modal} from "antd";
+import MenuBar from "../../components/MenuBar";
+import { ReactComponent as Line } from "../../icon/mainIcon/bars-solid.svg";
 const Main = () => {
   return (
     <div className="mainWindow">
-      <button className="mainMenu">메뉴</button>
-      {/* 메뉴 클릭시 도움말, 게임종료, 인벤토리*/}
-      <div className="nickname">닉네임 정보</div>
-      <div className="main">
-        <div className="startButton">
-          <button>모험 출발</button>
-        </div>
+      <div className="mainTop">
+        Ascend
+      <Line width="30" height="25" />
       </div>
-      <div className="gearStatus">
-        <div className="gear">
-          <div>투구</div>
-          {/* 각 무기별 인벤토리 */}
-          <div>갑옷</div>
-          <div>장갑</div>
-          <div>신발</div>
-          <div>무기</div>
-          <div>펫</div>
+      <MenuBar />
+      <div className="mainForm">
+        <div className="mainStart">
+          <div className="gameName">Ascend</div>
+          <div className="startButton">
+            <button className="startBtn">모험 출발</button>
+          </div>
         </div>
-        <div className="status">
-          체력 100 방어력 0 크리티컬 데미지 150% 크리티컬 확률 10% 기본 데미지
-          10 재화 0원 현재 ?층
-        </div>
-      </div>
-      <div className="shopButton">
-        <button>상점</button>
+        {/* <div className="mainSide">
+          <div className="gearStatus">
+            <div className="gear">
+              <div className="gear_1">
+                <div>투구</div>
+                <div>갑옷</div>
+              </div>
+              <div className="gear_2">
+                <div>장갑</div>
+                <div>신발</div>
+              </div>
+              <div className="gear_3">
+                <div>무기</div>
+              </div>
+            </div>
+            <div className="userStatus">
+              <div className="statusSt">
+                체력 100
+              </div>
+              <div className="statusDe">
+                방어력 0
+              </div>
+              <div className="statusCriDa">
+                크리티컬 데미지 150%
+              </div>
+              <div className="statusCriPro">
+                크리티컬 확률 10%
+              </div>
+              <div className="statusDa">
+                기본 데미지 10
+              </div>
+              <div className="statusMoney">
+                재화 0원
+              </div>
+              <div className="statusStage">
+                현재 ?층
+              </div>
+            </div>
+          </div>
+          <div className="shopButton">
+            <button className="shopBtn">상점</button>
+          </div>
+        </div> */}
       </div>
     </div>
   );

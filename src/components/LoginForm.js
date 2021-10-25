034582200bef2d0a18b56a46f99996595ecca5b9
Sch0co <tnqlsgyqls98@naver.com>
2021-10-25 23:04:16 +0900

    loginform update

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,10 +1,12 @@
 import React, { useState } from "react";
-import { Button } from "antd";
+import { Button, Tabs } from "antd";
 import "./LoginForm.css";
 import { useHistory } from "react-router-dom";
 
+const { TabPane } = Tabs;
+
 const LoginForm = () => {
-  const [loginTab, setLoginTab] = useState(true);
+  // const [loginTab, setLoginTab] = useState(true);
 
   const history = useHistory();
 
@@ -14,75 +16,81 @@ const LoginForm = () => {
 
   return (
     <div className="login">
-      <div className="formTitle">
-        <Button
+      {/* <div className="formTitle">
+        {<Button
           onClick={() => {
             setLoginTab(true);
           }}
           style={{
-            backgroundColor: loginTab ? "red" : "white",
+            backgroundColor: loginTab ? "#A6EDFF" : "white",
           }}
         >
-          Login
+          로그인
         </Button>
         <Button
           onClick={() => {
             setLoginTab(false);
           }}
           style={{
-            backgroundColor: loginTab ? "white" : "red",
+            backgroundColor: !loginTab ? "#A6EDFF" : "white",
           }}
         >
-          Sign up
+          회원가입
         </Button>
-      </div>
-      {loginTab ? (
-        <div className="loginStartForm">
-          <h3 className="loginStart">존재 확인, 접속 합니다.</h3>
-          <form className="loginForm">
-            <div className="textTab">
-              <span>ID</span>
-              <input type="text" name="userId" required />
-            </div>
-            <div className="textTab">
-              <span>PW</span>
-              <input type="password" name="userPw" required />
-            </div>
-            <div className="loginButton">
-              <Button className="onMain" type="submit" onClick={onLogin}>
-                접속
-              </Button>
-            </div>
-          </form>
-        </div>
-      ) : (
-        <div className="registStartForm">
-          <h3 className="loginStart signupStart">존재를 생성합니다.</h3>
-          <form className="loginForm signupForm">
-            <div className="textTab">
-              <span>ID</span>
-              <input type="text" name="userId" required />
-            </div>
-            <div className="textTab">
-              <span>PW</span>
-              <input type="password" name="userPw" required />
-            </div>
-            <div className="textTab">
-              <span>E-Mail</span>
-              <input type="email" name="userEmail" required />
-            </div>
-            <div className="textTab">
-              <span>Nickname</span>
-              <input type="text" name="userNickName" required />
-            </div>
-            <div className="loginButton">
-              <Button className="onMain" type="submit" onClick={onLogin}>
-                접속
-              </Button>
-            </div>
-          </form>
-        </div>
-      )}
+      </div> */}
+      {/* {loginTab ? ( */}
+      <Tabs className="loginTab" defaultActiveKey="1">
+        <TabPane tab={<div className="tabBtn">로그인</div>} key="1">
+          <div className="startForm">
+            <h3 className="loginStart">존재 확인, 접속 합니다.</h3>
+            <form className="loginForm">
+              <div className="textTab">
+                <span>ID</span>
+                <input type="text" name="userId" required />
+              </div>
+              <div className="textTab">
+                <span>PW</span>
+                <input type="password" name="userPw" required />
+              </div>
+              <div className="loginButton">
+                <button className="onMain" type="submit" onClick={onLogin}>
+                  접속
+                </button>
+              </div>
+            </form>
+          </div>
+        </TabPane>
+        {/* ) : ( */}
+        <TabPane tab={<div className="tabBtn">회원가입</div>} key="2">
+          <div className="startForm">
+            <h3 className="loginStart signupStart">존재를 생성합니다.</h3>
+            <form className="loginForm signupForm">
+              <div className="textTab">
+                <span>ID</span>
+                <input type="text" name="userId" required />
+              </div>
+              <div className="textTab">
+                <span>PW</span>
+                <input type="password" name="userPw" required />
+              </div>
+              <div className="textTab">
+                <span>E-Mail</span>
+                <input type="email" name="userEmail" required />
+              </div>
+              <div className="textTab">
+                <span>Nickname</span>
+                <input type="text" name="userNickName" required />
+              </div>
+              <div className="loginButton">
+                <button className="onMain" type="submit">
+                  생성
+                </button>
+              </div>
+            </form>
+          </div>
+        </TabPane>
+      </Tabs>
+      {/* )} */}
     </div>
   );
 };

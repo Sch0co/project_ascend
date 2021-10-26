8eb3967f13663617546f0a2e0f36a296df53845b
Sch0co <tnqlsgyqls98@naver.com>
2021-10-27 01:22:23 +0900

    sidemenubar update

--- src/components/MenuBar.js
+++ src/components/MenuBar.js
@@ -1,51 +1,146 @@
 import { React, useState } from "react";
 import "./MenuBar.css";
-import { ReactComponent as Exit } from "../icon/mainIcon/arrow-right-solid.svg";
+import { ReactComponent as Close } from "../icon/mainIcon/arrow-right-solid.svg";
+import { ReactComponent as Line } from "../icon/mainIcon/bars-solid.svg";
+import { ReactComponent as ModalClose } from "../icon/mainIcon/x_close_cross_delete_icon.svg";
+import { useMediaQuery } from "react-responsive"
+import { useHistory } from "react-router-dom";
+import Modal from "react-modal";
+
+const modalStyle = {
+    overlay: {
+        backgroundColor: "rgba(0, 0, 0, 0.5)",
+    },
+    content: {
+        backgroundColor: "#fff",
+    }
+}
 
 const MenuBar = () => {
     const [sideToggle, setSideToggle] = useState(true);
+    const [isMyPage, setIsMyPage] = useState(false);
+    const [isCharacter, setIsCharacter] = useState(false);
+    const [isShop, setIsShop] = useState(false);
+
+    const screenMove = useMediaQuery({
+        query: "(max-width: 600px)",
+    })
 
     const showBar = () => {
         setSideToggle(!sideToggle);
     }
 
+    const myPageOpen = () => {
+        setIsMyPage(true);
+    }
+
+    const myPageClose = () => {
+        setIsMyPage(false);
+    }
+    
+    const characterOpen = () => {
+        setIsCharacter(true);
+    }
+
+    const characterClose = () => {
+        setIsCharacter(false);
+    }
+    const shopOpen = () => {
+        setIsShop(true);
+    }
+
+    const shopClose = () => {
+        setIsShop(false);
+    }
+
+    const history = useHistory();
+
+    const onLogout = () => {
+        history.push("/");
+    }
+
     return (
         <>
-            { sideToggle && (
+            { sideToggle || !screenMove ? (
                 <div className="sideBarMenu">
                     <div className="sideBarTop">
-                        <div className="sideBarMenuName">MENU</div>
-                        <button
-                            className="sideBarExit"
-                            onClick={showBar}
-                        >
-                            <Exit width="15" height="15"/>
-                        </button>
+                        { screenMove &&
+                            <button
+                                className="sideBarClose"
+                                onClick={showBar}
+                            >
+                                <Close width="15" height="15"/>
+                            </button>
+                        }
                     </div>
                     <div className="sideBarList">
-                        <div>
-                            <button>
+                        <div className="myPage">
+                            <button
+                                onClick={myPageOpen}
+                            >
                                 마이페이지
                             </button>
                         </div>
+                        <Modal
+                            isOpen={isMyPage}
+                            onRequestClose={myPageClose}
+                            style={modalStyle}
+                        >
+                            <div className="myPageTop">
+                                <div>마이페이지</div>
+                                <ModalClose onClick={myPageClose} />
+                            </div>
+                            <div className="myPageList">
+                                <div>프로필 이미지 변경</div>
+                                <div>닉네임 변경</div>
+                                <div>비밀번호 변경</div>
+                                <div>회원 탈퇴</div>
+                            </div>
+                        </Modal>
                         <div>
-                            <button>
+                            <button onClick={characterOpen}>
                                 캐릭터 세팅
                             </button>
                         </div>
+                            <Modal
+                                isOpen={isCharacter}
+                                onRequestClose={characterClose}
+                                style={modalStyle}
+                            >
+                                캐릭터 세팅
+                                <ModalClose onClick={characterClose} />
+                            </Modal>
                         <div>
-                            <button>
+                            <button onClick={shopOpen}>
                                 상점
                             </button>
                         </div>
+                            <Modal
+                                isOpen={isShop}
+                                onRequestClose={shopClose}
+                                style={modalStyle}
+                            >
+                                상점
+                            <ModalClose onClick={shopClose} />
+                            </Modal>
                         <div>
-                            <button>
+                            <button onClick={onLogout}>
                                 로그아웃
                             </button>
                         </div>
                     </div>
                 </div>
-            )}
+            ) : (
+                    <div className="mainSide">
+                        <Line
+                            className="mainSideBtn"
+                            width="30"
+                            height="25"
+                            onClick={showBar}
+                        />
+                    </div>
+                )
+            }
         </>
     )
 }

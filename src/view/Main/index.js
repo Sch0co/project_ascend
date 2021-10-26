78def10c7e0b6f8dbf0567728b18df6775f2929d
Sch0co <tnqlsgyqls98@naver.com>
2021-10-27 01:22:08 +0900

    main view update

--- src/view/Main/index.js
+++ src/view/Main/index.js
@@ -2,26 +2,11 @@ import { React, useState } from "react";
 import "./index.css";
 import { Modal } from "antd";
 import MenuBar from "../../components/MenuBar";
-import { ReactComponent as Line } from "../../icon/mainIcon/bars-solid.svg";
 
 const Main = () => {
-  const [sideMenuToggle, setSideMenuToggle] = useState(true);
-
-  const menuToggle = () => {
-    setSideMenuToggle(!sideMenuToggle);
-  }
 
   return (
     <div className="mainWindow">
-      <div className="mainTop">
-        Ascend
-        <Line
-          className="mainSide"
-          width="30"
-          height="25"
-          onClick={menuToggle}  
-        />
-      </div>
       <div className="mainForm">
         <div className="mainStart">
           <div className="gameName">Ascend</div>
@@ -29,9 +14,7 @@ const Main = () => {
             <button className="startBtn">모험 출발</button>
           </div>
         </div>
-        {
-          sideMenuToggle && (<MenuBar />)
-        }
+        <MenuBar />
       </div>
     </div>
   );

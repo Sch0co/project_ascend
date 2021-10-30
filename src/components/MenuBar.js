9e2ead5060bba485a75a667acb977c5c1375d214
Sch0co <tnqlsgyqls98@naver.com>
2021-10-31 01:20:15 +0900

    sidebar menu update

--- src/components/MenuBar.js
+++ src/components/MenuBar.js
@@ -3,6 +3,11 @@ import "./MenuBar.css";
 import { ReactComponent as Close } from "../icon/mainIcon/arrow-right-solid.svg";
 import { ReactComponent as Line } from "../icon/mainIcon/bars-solid.svg";
 import { ReactComponent as ModalClose } from "../icon/mainIcon/x_close_cross_delete_icon.svg";
+import { ReactComponent as User } from "../icon/mainIcon/user.svg";
+import { ReactComponent as Hp } from "../icon/mainIcon/heart-solid.svg";
+import { ReactComponent as Armor } from "../icon/mainIcon/armor.svg";
+import { ReactComponent as Damage } from "../icon/mainIcon/swords.svg";
+import { ReactComponent as Coin } from "../icon/mainIcon/coins.svg";
 import { useMediaQuery } from "react-responsive"
 import { useHistory } from "react-router-dom";
 import Modal from "react-modal";
@@ -25,7 +30,6 @@ const characterStyle = {
     content: {
         backgroundColor: "#fff",
         maxWidth: "700px",
-        textAlign: "center",
         margin: "0 auto",
     }
 }
@@ -36,6 +40,21 @@ const shopStyle = {
     },
     content: {
         backgroundColor: "#fff",
+        maxWidth: "900px",
+        margin: "0 auto",
+    }
+}
+
+const gachaStyle = {
+    overlay: {
+        backgroundColor: "rgba(0, 0, 0, 0.5)",
+    },
+    content: {
+        backgroundColor: "#fff",
+        maxHeight: "400px",
+        maxWidth: "700px",
+        textAlign: "center",
+        margin: "auto auto",
     }
 }
 
@@ -44,7 +63,8 @@ const MenuBar = () => {
     // const [isMyPage, setIsMyPage] = useState(false);
     const [isCharacter, setIsCharacter] = useState(false);
     const [isShop, setIsShop] = useState(false);
-    const [upload, setUpload] = useState(null);
+    const [isOneGacha, setIsOneGacha] = useState(false);
+    const [isTenGacha, setIsTenGacha] = useState(false);
 
     const screenMove = useMediaQuery({
         query: "(max-width: 600px)",
@@ -63,6 +83,7 @@ const MenuBar = () => {
     //     setIsMyPage(false);
     // }
     
+    // madal open/close
     const characterOpen = () => {
         setIsCharacter(true);
     }
@@ -70,12 +91,29 @@ const MenuBar = () => {
     const characterClose = () => {
         setIsCharacter(false);
     }
+
     const shopOpen = () => {
         setIsShop(true);
     }
 
     const shopClose = () => {
         setIsShop(false);
+    }
+
+    const oneGachaOpen = () => {
+        setIsOneGacha(true);
+    }
+
+    const oneGachaclose = () => {
+        setIsOneGacha(false);
+    }
+
+    const tenGachaOpen = () => {
+        setIsTenGacha(true);
+    }
+
+    const tenGachaclose = () => {
+        setIsTenGacha(false);
     }
 
     // logout
@@ -153,7 +191,9 @@ const MenuBar = () => {
                                     <div className="characterSetList">
                                         <div className="charaSta">
                                             <div className="charaGear">
-                                                <div className="gearTop">캐릭터 장비</div>
+                                                <div className="gearTop">
+                                                    캐릭터 장비
+                                                </div>
                                                 <div className="gearTab">
                                                     <div className="gear">
                                                         <div>
@@ -172,19 +212,76 @@ const MenuBar = () => {
                                                 </div>
                                             </div>
                                             <div className="charaStatus">
-                                                <div className="statusTop">캐릭터 스탯</div>
+                                                <div className="statusTop">
+                                                    캐릭터 스탯
+                                                </div>
                                                 <div className="status">
-                                                    <div>체력 100</div>
-                                                    <div>방어력 0</div>
-                                                    <div>기본 데미지 10</div>
-                                                    <div>재화 0원</div>
-                                                    <div>캐릭터 생성 날짜</div>
+                                                    <div className="statusName">
+                                                        <div>
+                                                            <User
+                                                                width="16px"
+                                                                height="16px" 
+                                                                style={{
+                                                                    marginRight: "5px"
+                                                                }}
+                                                            />
+                                                            이름
+                                                        </div>
+                                                        <div>
+                                                            <Hp
+                                                                width="16px"
+                                                                height="16px"
+                                                                style={{
+                                                                    marginRight: "5px"
+                                                                }}  
+                                                            />
+                                                            체력
+                                                        </div>
+                                                        <div>
+                                                            <Armor
+                                                                width="16px"
+                                                                height="16px"
+                                                                style={{
+                                                                    marginRight: "5px"
+                                                                }}
+                                                            />
+                                                            방어력
+                                                        </div>
+                                                        <div>
+                                                            <Damage
+                                                                width="16px"
+                                                                height="16px"
+                                                                style={{
+                                                                    marginRight: "5px"
+                                                                }}
+                                                            />
+                                                            데미지
+                                                        </div>
+                                                        <div>
+                                                            <Coin
+                                                                width="16px"
+                                                                height="16px"
+                                                                style={{
+                                                                    marginRight: "5px"
+                                                                }}
+                                                            />
+                                                            재화
+                                                        </div>
+                                                        {/* <div>캐릭터 생성 날짜</div> */}
+                                                    </div>
+                                                    <div className="statusNum">
+                                                        <div>닉네임</div>
+                                                        <div>100</div>
+                                                        <div>0</div>
+                                                        <div>10</div>
+                                                        <div>0</div>
+                                                    </div>
                                                 </div>
                                             </div>
                                         </div>
                                         <div className="inventory">
-                                            <div>인벤토리</div>
-                                            <div> 표 </div>
+                                            <div className="invenTop">인벤토리</div>
+                                            <div className="invenArray"> 인벤 리스트 </div>
                                         </div>
                                     </div>
                                 </div>
@@ -206,12 +303,36 @@ const MenuBar = () => {
                                     </div>
                                     <div className="shopList">
                                         <div className="gacha">
-                                            <div>
-                                                1회 뽑기
+                                            <div className="gacha_1">
+                                                <div className="gacha_1_img">
+                                                    뽑기볼 이미지
+                                                </div>
+                                                <div className="gacha_1s">
+                                                    <button onClick={oneGachaOpen}>
+                                                        1회 뽑기
+                                                    </button>
+                                                </div>
                                             </div>
-                                            <div>
-                                                10회 뽑기
+                                            <Modal
+                                                isOpen={isOneGacha}
+                                                onRequestClose={oneGachaclose}
+                                                style={gachaStyle}
+                                            ></Modal>
+                                            <div className="gacha_2">
+                                                <div className="gacha_2_img">
+                                                    뽑기볼 이미지 * 10
+                                                </div>
+                                                <div className="gacha_10s">
+                                                    <button onClick={tenGachaOpen}>
+                                                        10회 뽑기
+                                                    </button>
+                                                </div>
                                             </div>
+                                            <Modal
+                                                isOpen={isTenGacha}
+                                                onRequestClose={tenGachaclose}
+                                                style={gachaStyle}
+                                            ></Modal>
                                         </div>
                                     </div>
                                 </div>

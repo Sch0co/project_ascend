6a626fcbbad40dfd0caa125deb04d84463f64ee4
Sch0co <tnqlsgyqls98@naver.com>
2021-11-02 11:10:22 +0900

    main view update

--- src/view/Main/index.js
+++ src/view/Main/index.js
@@ -1,20 +1,70 @@
 import { React, useState } from "react";
 import "./index.css";
-import { Modal } from "antd";
 import MenuBar from "../../components/MenuBar";
+import CharacterSet from "../../components/CharacterSet";
+import CharaStatus from "../../components/CharaStatus";
+import Modal from "react-modal";
+
+const characterStyle = {
+  overlay: {
+      backgroundColor: "rgba(0, 0, 0, 0.5)",
+  },
+  content: {
+      backgroundColor: "#fff",
+      maxWidth: "700px",
+      margin: "0 auto",
+  }
+}
 
 const Main = () => {
+  const [isCharacter, setIsCharacter] = useState(false);
+
+  const characterOpen = () => {
+    setIsCharacter(true);
+  }
+
+  const characterClose = () => {
+    setIsCharacter(false);
+  }
 
   return (
     <div className="mainWindow">
-      <div className="mainForm">
-        <div className="mainStart">
-          <div className="gameName">MonTap</div>
-          <div className="startButton">
-            <button className="startBtn">모험 출발</button>
+      <MenuBar />
+      <div className="mainView">
+        <div className="mainInfo">
+          <div className="mainInfoTop">
+            <div className="mainInfoStatus">
+              캐릭터 스탯
+            </div>
+            <CharaStatus />
+          </div>
+          <div className="mainSetting">
+            <div
+              className="mainIngoStaSet"
+              style={{
+                marginRight: 20
+              }}  
+            >
+              <button className="mainStaBtn" onClick={characterOpen}>
+                캐릭터 세팅
+              </button>
+            </div>
+            <Modal
+              isOpen={isCharacter}
+              onRequestClose={characterClose}
+              style={characterStyle}
+            >
+              <CharacterSet />
+            </Modal>
+            <div className="mainStartBtn">
+              <button className="startBtn">모험 출발</button>
+            </div>
           </div>
         </div>
-        <MenuBar />
+        <div className="mainItem">
+          <div className="mainItemTop">최근 획득 장비</div>
+          <div className="mainItemList">템 목록 ~~</div>
+        </div>
       </div>
     </div>
   );

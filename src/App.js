45a1f23e3e6185a64d97913957f85852dbc4c892
Sch0co <tnqlsgyqls98@naver.com>
2021-11-03 23:14:25 +0900

    route update

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import "antd/dist/antd.css";
 import React from "react";
 import Main from "./view/Main/";
 import Init from "./view/Init/index";
+import Game from "./view/Game/index"
 import { Route, Switch } from "react-router-dom";
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
     <Switch>
       <Route exact path="/" component={Init} />
       <Route exact path="/main" component={Main} />
+      <Route exact path="/main/game" component={Game} />
       {/* <LoginForm /> */}
     </Switch>
   );

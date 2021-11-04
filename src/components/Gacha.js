c6f87dfdbad78131886820ad234369537c3be89a
Sch0co <tnqlsgyqls98@naver.com>
2021-11-05 01:01:37 +0900

    gacha init

---
+++ src/components/Gacha.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./Gacha.css";
+
+const item = [
+    {
+        key: 1,
+    },
+    {
+        key: 2,
+    },
+]
+
+const Gacha = () => {
+    return (
+        <div>
+        </div>
+    )
+}
+
+export default Gacha;

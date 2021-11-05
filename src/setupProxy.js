2722ede28a8b6ae6e951ada29d4bc343cb3dc90b
Sch0co <tnqlsgyqls98@naver.com>
2021-11-06 01:00:49 +0900

    proxy change

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,9 +2,9 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function(app) {
   app.use(
-    '/api', // 불러오려는 server 의 api path
+    '/api', //server 의 api path
     createProxyMiddleware({
-      target: 'http://220.78.202.122:8090', // server 주소를 넣어주면 된다.
+      target: 'http://13.125.114.8:3306', // server 주소
       changeOrigin: true,
       secure: false
     })

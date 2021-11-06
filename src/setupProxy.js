6588b629ae1df7f9ecc825ee87c3b845ee2a9832
Sch0co <tnqlsgyqls98@naver.com>
2021-11-06 20:45:57 +0900

    server update

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function(app) {
   app.use(
     '/api', //server 의 api path
     createProxyMiddleware({
-      target: 'http://13.125.114.8:3306', // server 주소
+      target: 'http://Montap-env.eba-fnbxy2ap.ap-northeast-2.elasticbeanstalk.com', // server 주소
       changeOrigin: true,
       secure: false
     })

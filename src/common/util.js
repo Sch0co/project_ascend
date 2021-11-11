58c3e6e95c0b25d13ac816912c8329947144e701
Sch0co <tnqlsgyqls98@naver.com>
2021-11-11 21:00:35 +0900

    info delete

--- src/common/util.js
+++ src/common/util.js
@@ -1,3 +1,7 @@
 import React from "react";
 
-export const API_URL = process.env.NODE_ENV === "development" ? "" : "http://Montap-env.eba-fnbxy2ap.ap-northeast-2.elasticbeanstalk.com";
+export const API_URL = process.env.NODE_ENV === "development"
+    ?
+        "http://116.39.192.223:5000"
+    :
+        "http://Montap-env.eba-fnbxy2ap.ap-northeast-2.elasticbeanstalk.com";
